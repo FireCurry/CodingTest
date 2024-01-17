d5e376c5f193970025e6e57d26f9c9ac990528b1
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-01-17 22:10:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n) {
+        int num = n%2 == 0 ? n/2 : n/2 +1;
+        
+        int[] answer = new int[num];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = 2*i+1;
+        }
+        return answer;
+    }
+}
