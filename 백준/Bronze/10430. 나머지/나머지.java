8c179e442eea155096aa7a8e3dda356a1a5e3076
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-03-04 16:31:22 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+        System.out.println((a+b)%c);
+        System.out.println(((a%c) + (b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c) * (b%c))%c);
+    }
+}
