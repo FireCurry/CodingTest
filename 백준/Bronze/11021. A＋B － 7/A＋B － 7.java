bfbcac3f0fc0fe4c3f4e8a49852e46c630f3c63f
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-03-06 17:18:07 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16148 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int number = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=number; i++){
+            String[] arr = br.readLine().split(" ");
+            bw.write("Case #" + i + ": " + (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1])) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
