49cfc98e9cfdd53b7af0fb49e4b596a71c5d0357
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-03-06 17:27:19 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int number = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<number; i++){
+            for(int u=0; u<=i; u++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
