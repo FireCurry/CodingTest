97e8d854239c063dc93cccb0718c84c6af07b767
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-03-06 17:12:28 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1024 ms, Memory: 289808 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int number = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<number; i++){
+            String[] arr = br.readLine().split(" ");
+            
+            bw.write(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) + "\n");
+            
+        }
+        bw.flush();
+        bw.close();
+    }
+}
