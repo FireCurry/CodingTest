c079f5ce71c057cee7e850f80f8d0d5b23e9272f
FireCurry <141403823+FireCurry@users.noreply.github.com>
2024-03-27 14:20:23 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.28 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String my_string) {
+        
+        
+        String[] answer = my_string.trim().split("\\s+");
+        return answer;
+    }
+}
