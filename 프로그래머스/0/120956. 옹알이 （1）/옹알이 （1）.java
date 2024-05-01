bea690a6d57e746ec5958d60ae5719b7249bb26b
김민석 <141403823+FireCurry@users.noreply.github.com>
2024-05-01 17:43:11 +0900

    [level 0] Title: 옹알이 (1), Time: 0.60 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,10 +3,6 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<babbling.length; i++){
-            if(babbling[i].indexOf("aya") != babbling[i].lastIndexOf("aya")) continue;
-            if(babbling[i].indexOf("ma") != babbling[i].lastIndexOf("ma")) continue;
-            if(babbling[i].indexOf("ye") != babbling[i].lastIndexOf("ye")) continue;
-            if(babbling[i].indexOf("woo") != babbling[i].lastIndexOf("woo")) continue;
             if(babbling[i].replaceAll("aya|ma|ye|woo", "").length() == 0) answer++; 
         }
         
