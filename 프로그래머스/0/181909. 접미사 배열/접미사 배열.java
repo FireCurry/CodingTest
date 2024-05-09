9c5906ef7a5536a55bcf7f4a613dcdc56f86be15
김민석 <141403823+FireCurry@users.noreply.github.com>
2024-05-09 16:26:16 +0900

    [level 0] Title: 접미사 배열, Time: 0.26 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = new String[my_string.length()];
+        StringBuilder str = new StringBuilder(my_string);
+        
+        for(int i=0; i<answer.length; i++){
+            
+            answer[i] = str.toString();
+            str.deleteCharAt(0);
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
