98da68a263fa24afc04585ad0cd422c25d49889a
김민석 <141403823+FireCurry@users.noreply.github.com>
2024-05-09 16:00:06 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 22.79 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        if(myString.charAt(0) == 'x'){
+            myString = myString.replaceFirst("x+", "");
+        }
+        String[] answer = myString.split("x+");
+        Arrays.sort(answer);
+        return answer;
+    }
+}
